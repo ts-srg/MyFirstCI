5463914bc254412675514e6f76d9328fe2efb6c5
ts <t.sviridenko@icloud.com>
2026-02-24 19:46:55 +0300

    add new test for proj

--- MyFirstCI.Api.Test/UnitTest1.cs
+++ MyFirstCI.Api.Test/UnitTest1.cs
@@ -30,4 +30,16 @@ public class UnitTest1
         }
 
     }
+
+    [Fact]
+    public void Get_ReturnTenValue()
+    {
+        //Arrange
+        var controller = new WeatherController();
+        //Act
+        var result = controller.Get();
+        //Assert
+        Assert.Equal(10, result.Length);
+
+    }
 }
